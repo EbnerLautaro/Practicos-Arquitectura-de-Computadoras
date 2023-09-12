1ae15e897947abee459b599ce2f74dc84679fa61
Ebner Lautaro <lautaro.ebner@mi.unc.edu.ar>
2023-09-11 22:27:38 -0300

    controller y maindec avances

--- modules/controller.sv
+++ modules/controller.sv
@@ -4,22 +4,37 @@ module controller
 (
 	input logic [10:0] instr,
 	output logic [3:0] AluControl,						
-	output logic reg2loc, regWrite, AluSrc, Branch,memtoReg, memRead, memWrite
+	output logic reg2loc, regWrite, Branch, memtoReg, memRead, memWrite,
+	
+	
+	// Señales nuevas
+	input logic reset, ExtIRQ, ExcAck,
+	output logic ExtIAck, ERet, Exc,
+	output logic [3:0] EStatus,
+	output logic [1:0] AluSrc // modificada
 );
+	logic NotAnInstr; // Señales nuevas
 											
-	logic [1:0] AluOp_s;
+	logic [1:0] AluOp_s; 
 											
 	maindec decPpal (
 		.Op(instr), 
+		.reset(reset),
 		.Reg2Loc(reg2loc), 
-		.ALUSrc(AluSrc), 
 		.MemtoReg(memtoReg), 
 		.RegWrite(regWrite), 
 		.MemRead(memRead), 
 		.MemWrite(memWrite), 
 		.Branch(Branch), 
 		.ALUOp(AluOp_s)
+		.ALUSrc(AluSrc), 
+		.NotAnInstr(NotAnInstr),
+		.ERet(ERet),
+		.EStatus(EStatus)
 	);
+	
+	
+	
 	
 	aludec decAlu (
 		.funct(instr), 
@@ -27,4 +42,28 @@ module controller
 		.alucontrol(AluControl)
 	);
 			
+			 
+	always_comb begin 
+		Exc <= ExtIRQ | NotAnInstr;
+		
+		if (ExcAck && ExtIRQ)
+			ExtIAck <= 1;
+		else 
+			ExtIAck <= 0;
+	end 
+	
 endmodule
+
+
+
+/*
+[x] - Agregar la entrada reset al bloque #maindec. Si esta vale ‘1’, todas las salidas del bloque #controller deben valer ‘0’.
+[x] - Agregar un puerto de salida NotAnInstr al módulo #maindec, de tal forma que este tome el valor ‘1’ cuando se ingresa un opcode invalido, 
+	caso contrario debe valer ‘0’. El resto de las señales de control deben tomar los valores indicados en la tabla correspondientes a la fila “Invalid OpCode”.
+[x] - La salida Exc de #controller debe ser el resultado de una operación OR entre la entrada ExtIRQ y la señal interna NotAnInstr.
+[?] - Ante la ocurrencia de una flanco ascendente en ExtIRQ (indicada en la tabla por la fila “External IRQ”) las señales de control generadas en #maindec no sufren modificaciones.
+[x] - Las señales EStatus deben tomar los valores indicados en la tabla, según el caso correspondiente.
+[x] - La salida del modulo #controller, ExtIAck es ‘1’ cuando ExcAck = ‘1’ y ExtIRQ = ‘1’, caso contrario debe valer ‘0’.
+	
+	
+*/
--- modules/maindec.sv
+++ modules/maindec.sv
@@ -3,75 +3,114 @@
 module maindec
 (
 		input logic [10:0] Op,
-		
-		output logic Reg2Loc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,
-		output logic [1:0] ALUOp
+		input logic reset, // nuevo
+		output logic Reg2Loc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, 
+		output logic NotAnInstr, ERet, // nuevo
+		output logic [1:0] ALUOp, ALUSrc, // modificado
+		output logic [3:0] EStatus // nuevo
 );
 
 	always_comb begin
-		casez(Op)
-		
-		// LDUR
-		11'b111_1100_0010: begin 
-				Reg2Loc 	<= 1'b0;
-				ALUSrc 		<= 1'b1;
-				MemtoReg 	<= 1'b1;
-				RegWrite 	<= 1'b1;
-				MemRead 	<= 1'b1;
-				MemWrite 	<= 1'b0;
-				Branch 		<= 1'b0;
-				ALUOp 		<= 2'b00;
-		end
-			
-		// STUR
-		11'b111_1100_0000: begin  
-				Reg2Loc 	<= 1'b1;
-				ALUSrc 		<= 1'b1;
-				MemtoReg 	<= 1'b0;
-				RegWrite 	<= 1'b0;
-				MemRead 	<= 1'b0;
-				MemWrite 	<= 1'b1;
-				Branch 		<= 1'b0;
-				ALUOp 		<= 2'b00;
-		end
-		
-		// CBZ
-		11'b101_1010_0???: begin  
-				Reg2Loc 	<= 1'b1;
-				ALUSrc 		<= 1'b0;
-				MemtoReg 	<= 1'b0;
-				RegWrite 	<= 1'b0;
-				MemRead 	<= 1'b0;
-				MemWrite 	<= 1'b0;
-				Branch 		<= 1'b1;
-				ALUOp 		<= 2'b01;
-		end
-		
-		// ADD, SUB, AND, ORR 
-		11'b100_0101_1000, 11'b110_0101_1000, 11'b100_0101_0000, 11'b101_0101_0000: begin 
-				Reg2Loc 	<= 1'b0;
-				ALUSrc		<= 1'b0;
-				MemtoReg 	<= 1'b0;
-				RegWrite 	<= 1'b1;
-				MemRead 	<= 1'b0;
-				MemWrite 	<= 1'b0;
-				Branch 		<= 1'b0;
-				ALUOp 		<= 2'b10;
-		end
-		
-		default: begin
-				Reg2Loc 	<= 1'b0;
-				ALUSrc 		<= 1'b0;
-				MemtoReg 	<= 1'b0;
-				RegWrite	<= 1'b0;
-				MemRead 	<= 1'b0;
-				MemWrite 	<= 1'b0;
-				Branch 		<= 1'b0;
-				ALUOp 		<= 2'b00;
-		end
-		
-		endcase
 	
-	end
+		if (reset) begin 
+			Reg2Loc 	<= 1'b0;
+			MemtoReg 	<= 1'b0;
+			RegWrite 	<= 1'b0;
+			MemRead 	<= 1'b0;
+			MemWrite 	<= 1'b0;
+			Branch 		<= 1'b0;
+			ALUOp 		<= 2'b00;
+			ALUSrc		<= 2'b00;
+			ERet		<= 1'b0;
+			EStatus 	<= 4'b0000;
+
+		
+		end else begin 
+		
+			casez(Op)
+		
+				// LDUR
+				11'b111_1100_0010: begin 
+					Reg2Loc 	<= 1'bx;
+					ALUSrc 		<= 2'b01;
+					MemtoReg 	<= 1'b1;
+					RegWrite 	<= 1'b1;
+					MemRead 	<= 1'b1;
+					MemWrite 	<= 1'b0;
+					Branch 		<= 1'b0;
+					ALUOp 		<= 2'b00;
+					ERet		<= 1'b0;
+					EStatus 	<= 4'b0000;
+					NotAnInstr	<= 1'b0;
+				end
+					
+				// STUR
+				11'b111_1100_0000: begin   
+					Reg2Loc 	<= 1'b1;
+					ALUSrc 		<= 2'b01;
+					MemtoReg 	<= 1'bx;
+					RegWrite 	<= 1'b0;
+					MemRead 	<= 1'b0;
+					MemWrite 	<= 1'b1;
+					Branch 		<= 1'b0;
+					ALUOp 		<= 2'b00;
+					ERet		<= 1'b0;
+					EStatus 	<= 4'b0000;
+					NotAnInstr	<= 1'b0;
+				end
+				
+				// CBZ
+				11'b101_1010_0???: begin   
+					Reg2Loc 	<= 1'b1;
+					ALUSrc 		<= 2'b00;
+					MemtoReg 	<= 1'bx;
+					RegWrite 	<= 1'b0;
+					MemRead 	<= 1'b0;
+					MemWrite 	<= 1'b0;
+					Branch 		<= 1'b1;
+					ALUOp 		<= 2'b01;
+					ERet		<= 1'b0;
+					EStatus 	<= 4'b0000;
+					NotAnInstr	<= 1'b0;
+				end
+				
+				// R-TYPE
+				// ADD, SUB, AND, ORR 
+				11'b100_0101_1000, 11'b110_0101_1000, 11'b100_0101_0000, 11'b101_0101_0000: begin 
+					Reg2Loc 	<= 1'b0;
+					ALUSrc 		<= 2'b00;
+					MemtoReg 	<= 1'b0;
+					RegWrite 	<= 1'b1;
+					MemRead 	<= 1'b0;
+					MemWrite 	<= 1'b0;
+					Branch 		<= 1'b0;
+					ALUOp 		<= 2'b10;
+					ERet		<= 1'b0;
+					EStatus 	<= 4'b0000;
+					NotAnInstr	<= 1'b0;
+					
+				end
+				
+				
+				
+				
+				// INVALID OPCODE
+				default: begin 
+					Reg2Loc 	<= 1'bx;
+					ALUSrc 		<= 2'bxx;
+					MemtoReg 	<= 1'b0;
+					RegWrite 	<= 1'b0;
+					MemRead 	<= 1'b0;
+					MemWrite 	<= 1'b0;
+					Branch 		<= 1'b0;
+					ALUOp 		<= 2'bxx;
+					ERet		<= 1'b0;
+					EStatus 	<= 4'b0010;
+					NotAnInstr	<= 1'b1;
+				end
+			
+			endcase
+		end // else
+	end // always_comb 
 	
 endmodule 
